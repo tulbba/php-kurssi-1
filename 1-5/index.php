8d7b2fefe4e1817ac94629537b93cd2eff48c49e
tulbba <73821881+tulbba@users.noreply.github.com>
2020-11-03 11:09:58 +0200

    Update index.php

--- 1-5/index.php
+++ 1-5/index.php
@@ -1,1 +1,37 @@
 <?php
+$letter[0] = "A ";
+$letter[1] = "B ";
+$letter[2] = "C ";
+$letter[3] = "D ";
+$letter[4] = "E ";
+$letter[5] = "F ";
+$letter[6] = "G ";
+$letter[7] = "H ";
+$letter[8] = "I ";
+$letter[9] = "J ";
+$letter[10] = "K ";
+
+echo $letter[0];
+echo $letter[1];
+echo $letter[2];
+echo $letter[3];
+echo $letter[4];
+echo $letter[5];
+echo $letter[6];
+echo $letter[7];
+echo $letter[8];
+echo $letter[9];
+echo $letter[10];
+echo "<br>";
+echo $letter[10];
+echo $letter[9];
+echo $letter[8];
+echo $letter[7];
+echo $letter[6];
+echo $letter[5];
+echo $letter[4];
+echo $letter[3];
+echo $letter[2];
+echo $letter[1];
+echo $letter[0];
+?>
