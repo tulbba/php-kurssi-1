2fc187cbdf99a87a50f6c7193b8904e215761595
tulbba <73821881+tulbba@users.noreply.github.com>
2020-11-03 12:46:31 +0200

    Update index.php

--- 1-7/index.php
+++ 1-7/index.php
@@ -1,1 +1,17 @@
 <?php
+$array = [
+    ['A ', 'B ', 'C'],
+    ['X ', 'Y ', 'Z'],
+    ['1 ', '2 ', '3']
+    ];
+    echo $array[0][0]; 
+    echo $array[0][1]; 
+    echo $array[0][2];
+    echo "<br>";
+    echo $array[1][0];
+    echo $array[1][1];
+    echo $array[1][2];
+    echo "<br>";
+    echo $array[2][0];
+    echo $array[2][1];
+    echo $array[2][2];
